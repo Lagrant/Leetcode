8b7f91985798fa9d184937835eabb8e12f24bb56
Lagrant <869874928@qq.com>
2024-11-09 13:00:47 -0500

    Nov 6

--- google/NextPermutation.py
+++ google/NextPermutation.py
@@ -1,9 +1,10 @@
+from typing import List
 class Solution:
     def nextPermutation(self, nums) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums) - 2, 0-1, -1):
+        for i in range(len(nums) - 2, -1, -1):
             if (nums[i] < nums[i+1]):
                 for j in range(len(nums) - 1, i, -1):
                     if (nums[j] > nums[i]):
@@ -13,4 +14,18 @@ class Solution:
                 break
             elif (i == 0):
                 nums.sort()
-        
+    
+    def nextPermutation1(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while j > i and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1 :] = sorted(nums[i + 1 :])
